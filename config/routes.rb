0ef5e818bb64b8738eb1049200b9e6e0c6b37045
David E Lares S <david.e.lares@gmail.com>
2020-02-21 00:27:20 -0400

    Adding articles resource routing and Controller - Testing Routing section created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # adding a resource route (full CRUD)
+  resources :articles, only: [:index]
 end
