0da4cf4e8a177299bf14ac614880b328b6c6081e
David E Lares S <david.e.lares@gmail.com>
2020-03-08 18:24:58 -0400

    Exposing Login Endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # exponse endpoints
+  post '/login', to: 'access_tokens#create'
   # adding a resource route (full CRUD)
   resources :articles, only: [:index, :show]
 end
